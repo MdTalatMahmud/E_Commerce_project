c2ac5a467f2ed4f7a7bd622e9a3b346179789b0a
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2019-12-30 00:05:15 +0600

    First commit

---
+++ E_Commerce_project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_Commerce_project.MvcApplication" Language="C#" %>
---
+++ E_Commerce_project/Models/CategoryDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce_project.Models
+{
+    public class CategoryDetail
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+    }
+}
