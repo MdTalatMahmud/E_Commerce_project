179bc90e74ac7c8bf79143cca48fbcd70c705c96
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-01-09 18:15:49 +0600

    update

---
+++ E_Commerce_project/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_Commerce_project.DAL;
+using E_Commerce_project.Repository;
+
+namespace E_Commerce_project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public GenericUnitOfWork _unitOfWork=new GenericUnitOfWork();
+        // GET: Dashboard
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>()
+                .GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allcategories);
+        }
+    }
+}
--- E_Commerce_project/Models/CategoryDetail.cs
+++ E_Commerce_project/Models/CategoryDetail.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace E_Commerce_project.Models
 {
     public class CategoryDetail
     {
         public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Category name required")]
+        [StringLength(100,ErrorMessage = "Minimum 3 and minimum 5 and maximum 100 characters are allowed ",MinimumLength = 3)]
         public string CategoryName { get; set; }
         public Nullable<bool> IsActive { get; set; }
         public Nullable<bool> IsDelete { get; set; }
+    }
+
+    public class ProductDetail
+    {
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "Product name is required")]
+        [StringLength(100, ErrorMessage = "Minimum 3 and minimum 5 and maximum 100 characters are allowed ", MinimumLength = 3)]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(1,50)]
+        public Nullable<int> CategoryId { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public Nullable<System.DateTime> Description { get; set; }
+        public string ProductImage { get; set; }
+        public Nullable<bool> IsFeatured { get; set; }
+        [Required]
+        [Range(typeof(int),"1","500",ErrorMessage = "Invalid quantity")]
+        public Nullable<int> Quantity { get; set; }
+        [Required]
+        [Range(typeof(decimal),"1","200000",ErrorMessage = "Invalid price")]
+        public Nullable<decimal> Price { get; set; }
+        public SelectList Categories { get; set; }
     }
 }
